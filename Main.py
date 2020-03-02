545b2a254429e2e4580f88388d4159fe63ce627a
sean <smgelbgithub@gmail.com>
2020-03-02 03:03:36 -0500

    push 4. Ill try to run it tomorrow. Getting gmail issues....

--- Main.py
+++ Main.py
@@ -1,20 +1,18 @@
-import Passwords
-import Stonks
-import threading
-import datetime
 import schedule
-import time
+import Stonks
 import AutomateEmail
+import Passwords
 
 def main():
     my_stock_list = {'GM', 'XOM', 'OVV', 'TSLA', 'AAPL', 'MMM'}
+    print("Starting...")
     test = Stonks.Stonks()
     for stock in my_stock_list:
         test.price(stock)
+    print("Schedule")
+    schedule.every(15).seconds.do(AutomateEmail.automaticEmail("does this work", "when turned off"))
 
     schedule.every().day.at("09:30").do(test.update_prices())
 
 
-
 main()
-
